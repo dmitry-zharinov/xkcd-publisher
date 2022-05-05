ebde76f5989ea4d524a27eb26f37b1c2b3f044db
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-05-05 15:43:59 +0300

    Реализовал публикацию в группе ВК

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import requests
 
 from utils import download_image, get_extension_from_url
-
+from publish_vk import upload_photo, wall_post
 IMG_FOLDER_NAME = 'images'
 
 
@@ -19,10 +19,17 @@ def main():
     
     print(xkcd['alt'])
     img_url = xkcd['img']
+    filename = f'xkcd{get_extension_from_url(img_url)}'
     download_image(img_url,
-                   f'xkcd{get_extension_from_url(img_url)}',
+                   filename,
                    IMG_FOLDER_NAME,
                    '')
 
+    VK_GROUP_ID = '213092935'
+    photo = upload_photo(VK_GROUP_ID, IMG_FOLDER_NAME, filename)
+    print(photo)
+    wall_post(VK_GROUP_ID, photo)
+    
+    
 if __name__ == '__main__':
     main()
--- publish_vk.py
+++ publish_vk.py
@@ -2,13 +2,15 @@ from tokenize import group
 from dotenv import load_dotenv
 import requests
 import os
+from pathlib import Path
 
 load_dotenv()
 TOKEN = os.environ['VK_ACCESS_TOKEN']
 CLIENT_ID = os.environ['VK_CLIENT_ID']
 API_VERSION = '5.131'
 
-def get_vk_upload_server(group_id):
+def get_vk_upload_url(group_id):
+    """Получить адрес для загрузки фото"""
     params = {
         'access_token': TOKEN,
         'v': API_VERSION,
@@ -19,11 +21,52 @@ def get_vk_upload_server(group_id):
         params=params
     )
     response.raise_for_status()
-    return response.json()
+    return response.json()['response']['upload_url']
     
-def publish_to_vk_group():
-    pass
+def upload_photo(group_id, filepath, filename):
+    """Загрузить фото на сервер"""
+    url = get_vk_upload_url(group_id)
+    photo_path = Path(filepath) / filename
+    files = {
+        'photo': (filename, open(photo_path, 'rb'))
+    }
+    response_post = requests.post(url, files=files)
+    response_post.raise_for_status()
+    
+    photo_upload = response_post.json()
+    params = {
+        'access_token': TOKEN,
+        'v': API_VERSION,
+        'group_id': group_id,
+        'photo': photo_upload['photo'],
+        'server': photo_upload['server'],
+        'hash': photo_upload['hash'],
+    }
+    response = requests.get(
+        'https://api.vk.com/method/photos.saveWallPhoto',
+        params=params
+    )
+    response.raise_for_status()
+
+    return response.json()['response']
 
 
-test = get_vk_upload_server('213092935')
-print(test)
+def wall_post(group_id, photo):
+    photo = photo[0]
+    photo = 'photo'+ '-' + str(photo['owner_id']) + '_' + str(photo['id'])
+    params = {
+        'access_token': TOKEN,
+        'v': API_VERSION,
+        'attachments': photo,
+        'message': 'Просто текст...',
+        'owner_id': '-' + group_id,
+        'from_group': '1'
+    }
+    response = requests.get(
+        'https://api.vk.com/method/wall.post',
+        params=params
+    )
+    response.raise_for_status()
+
+# test = publish_to_vk_group('213092935')
+# print(test)
